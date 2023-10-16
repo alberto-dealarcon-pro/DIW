274a88d24da8e228a6697c1ff5d7063acbc2fbf2
Alberto de Alarcón <alberto.dealarcon.pro@iesmarquesdecomares.org>
2023-10-16 18:51:58 +0200

    oih

--- framework-php-bootstrap/includes/navigation.php
+++ framework-php-bootstrap/includes/navigation.php
@@ -4,8 +4,7 @@
         <button class="navbar-toggler text-uppercase font-weight-bold text-white rounded" type="button"
             data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive"
             aria-expanded="false" aria-label="Toggle navigation">
-            Menu
-            <i class="fas fa-bars"></i>
+            <i class="fas fa-bars fa-2x"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
 
