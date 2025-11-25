ac65d1ddc48f12eac11ee5f6d367e0490a4692c6
Alberto de Alarcón <alberto.dealarcon.pro@iesmarquesdecomares.org>
2025-11-25 09:27:39 +0100

    Hola

--- Resueltos/Tema3/Video/bootstrap-videoplayer/js/example.js
+++ Resueltos/Tema3/Video/bootstrap-videoplayer/js/example.js
@@ -1,10 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    var player1 = new BootstrapVideoplayer('player-1',{
-        selectors:{
-            video: '.video'
-        }
-    })
+    var player1 = new BootstrapVideoplayer('player-1')
     var player2 = new BootstrapVideoplayer('player-2')
     var player3 = new BootstrapVideoplayer('player-3')
     var player4 = new BootstrapVideoplayer('player-4')
@@ -12,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     var player6 = new BootstrapVideoplayer('player-6')
     var player7 = new BootstrapVideoplayer('player-7')
     var player8 = new BootstrapVideoplayer('player-8')
-    var player8 = new BootstrapVideoplayer('player-9')
+    var player9 = new BootstrapVideoplayer('player-9')
 
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
